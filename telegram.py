e9802331287d318de7887a1e7a38e61ae50bca5d
Romain <romain.gilliot@motion-live.com>
2026-03-17 10:02:07 +0100

    added the service-monitor tool and the telegram python api

---
+++ telegram.py
@@ -0,0 +1,26 @@
+import socket
+import requests
+import time
+
+class Bot : 
+    def __init__(self, token) : 
+        self.token = token
+
+    def route(self) : 
+        return "https://api.telegram.org/bot" + self.token + "/"
+
+    #data is in dictionnary format here - json like
+    def request(self, function, data): 
+        url = self.route() + function
+        r = requests.post(url, json = data)
+        return r.json()
+
+    def sendMessage(self, chatId, content) : 
+        data = {
+            "chat_id" : chatId,
+            "text" : content
+        }
+        return self.request("sendMessage", data)
+
+    def getUpdates(self): 
+        return self.request("getUpdates", {})
